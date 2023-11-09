37749fce6946cc3e8470212e1fb57c44834b6bac
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-11-09 19:31:33 +0300

    changes

---
+++ OOP2/CorporateCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class CorporateCustomer
+    {
+        public int Id { get; set; }
+        public string CustomerNo { get; set; }
+        public string CompanyName { get; set; }
+        public string TaxNo { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nergis Ketenci
+            Customer customer1 = new Customer();
+            customer1.Name = "Nergis";
+            customer1.LastName = "Ketenci";
+            customer1.Id = 1;
+            customer1.TCNo = "11111111111";
+            customer1.CustomerNo = "12345";
+            customer1.CompanyName = "?"; //Müşteri adı ile alakalı değil
+
+            //Geçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            //Eğer bir veri üzerinde matematiksel herhangi bir işlem yapılmayacaksa verinin metinsel bir türde saklanması daha iyi olur.
+            //**Eğer bir değer o nesneye ait değilmiş gibi duruyorsa soyutlama hatası yapıyorsun demektir.
+
+            Console.ReadKey();
+        }
+    }
+}
