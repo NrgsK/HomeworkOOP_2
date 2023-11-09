14890d44f62b9527978be77112cf7511adeee8c5
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-11-09 17:18:21 +0300

    changes

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -19,15 +19,15 @@ namespace GenericsIntro
         public void Add(T item)
         {
             T[] tempArray = items; //Referans numarası kaybolmasın diye geçici bir dizi tanımlanıp items dizisinin referans adresi buraya atanır. Böylelikle items dizindeki elemanlar kaybolmaz.
-            items=new T[items.Length+1];
-            
+            items = new T[items.Length + 1];
+
             for (int i = 0; i < tempArray.Length; i++)
             {
                 items[i] = tempArray[i];
                 // Geçici dizideki elemanları geri alma
             }
 
-            items[items.Length-1] = item;
+            items[items.Length - 1] = item;
         }
     }
 }
