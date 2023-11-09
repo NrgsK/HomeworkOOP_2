48f6bc0d28b8db3fff62f2b4bd791fbe65d9bd14
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-11-09 22:03:54 +0300

    changes

---
+++ OOP3/ApplicationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class ApplicationManager
+    {
+        public void Apply(ILoanManager loanManager)
+        {
+            //Başvuran bilgilerini değerlendirme
+            //MortgageLoanManager mortgageLoanManager = new MortgageLoanManager(); // Doğru bir yöntem değil.
+
+            loanManager.Calculate();
+
+        }
+
+        public void DoLoanPreInformation(List<ILoanManager> loans)
+        {
+            foreach (var loan in loans)
+            {
+                loan.Calculate();
+            }
+        }
+
+    }
+}
--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -10,6 +10,23 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
+            ILoanManager personalLoanManager = new PersonalLoanManager();
+
+            VehicleLoanManager vehicleLoanManager = new VehicleLoanManager();
+
+            MortgageLoanManager mortgageLoanManager = new MortgageLoanManager();
+
+
+            //interface'lerde implemente edilen sınıfın referans numarasını tutar.
+
+            ApplicationManager applicationManager = new ApplicationManager();
+            //applicationManager.Apply(personalLoan);
+
+            List<ILoanManager> loans = new List<ILoanManager>() { personalLoanManager,vehicleLoanManager};
+            applicationManager.DoLoanPreInformation(loans);
+
+
+            Console.ReadKey();
         }
     }
 }
